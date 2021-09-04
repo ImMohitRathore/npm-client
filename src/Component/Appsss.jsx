2feb32c0f85b55588e693f77c4e52487de2edd9e
mohit rathore <hackside715@gmail.com>
2021-09-04 10:04:49 -0400

    changes

--- src/Component/Appsss.jsx
+++ src/Component/Appsss.jsx
@@ -18,7 +18,7 @@ const App =()=>{
      
   const check= async()=>{
      try{
-         const res = await fetch("https://mernmernmenr.herokuapp.com/dairy",{
+         const res = await fetch("/dairy",{
              method : "GET",
              headers:{
                  Accept : "application/json",
